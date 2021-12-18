0979cc58e1907523ea085ecfc20e8719f7a29492
Dmytro <dmvs.9898@gmail.com>
2021-12-18 14:52:24 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,8 +8,15 @@ const refs = {
   message: document.querySelector('.feedback-form textarea'),
 };
 
+// const feedBack = {
+//   email: refs.email.value,
+//   message: refs.message.value,
+// };
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
 // загружаем значения из хранилища если в нем что-то есть
-if (localStorage.getItem('email') || localStorage.getItem('message')) {
+if (localStorage.getItem(LOCALSTORAGE_KEY)) {
   loader();
 }
 
@@ -17,30 +24,47 @@ refs.form.addEventListener('input', throttle(onSaveInput, 500));
 refs.form.addEventListener('submit', onSendSubmit);
 
 function onSaveInput(e) {
-  if (e.target.nodeName === 'INPUT') {
-    localStorage.setItem('email', JSON.stringify(e.target.value));
-  } else if (e.target.nodeName === 'TEXTAREA') {
-    localStorage.setItem('message', JSON.stringify(e.target.value));
-  }
+  const feedBack = {
+    email: refs.email.value,
+    message: refs.message.value,
+  };
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedBack));
+
+  // if (e.target.nodeName === 'INPUT') {
+  //   localStorage.setItem('email', JSON.stringify(e.target.value));
+  // } else if (e.target.nodeName === 'TEXTAREA') {
+  //   localStorage.setItem('message', JSON.stringify(e.target.value));
+  // }
 }
 
 function onSendSubmit(e) {
   e.preventDefault();
 
-  const send = {
-    email: JSON.parse(localStorage.getItem('email')),
-    message: JSON.parse(localStorage.getItem('message')),
-  };
-  console.log(JSON.stringify(send));
-  //очистим поля ввода после Сабмита
+  console.log(localStorage.getItem(LOCALSTORAGE_KEY));
   e.currentTarget.reset();
-  //очистим хранилище после Сабмита, чтобы данные не остались в localStorage
-  localStorage.removeItem('email');
-  localStorage.removeItem('message');
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
 }
+
+// function onSendSubmit(e) {
+//   e.preventDefault();
+
+//   const send = {
+//     email: JSON.parse(localStorage.getItem('email')),
+//     message: JSON.parse(localStorage.getItem('message')),
+//   };
+//   console.log(JSON.stringify(send));
+//   //очистим поля ввода после Сабмита
+//   e.currentTarget.reset();
+//   //очистим хранилище после Сабмита, чтобы данные не остались в localStorage
+//   localStorage.removeItem('email');
+//   localStorage.removeItem('message');
+// }
 
 function loader() {
   // Вернем значение формы если не было Сабмита
-  refs.email.value = JSON.parse(localStorage.getItem('email'));
-  refs.message.value = JSON.parse(localStorage.getItem('message'));
+
+  refs.email.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email;
+  refs.message.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message;
 }
